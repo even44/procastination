8a691ca66e7709af126b503376d592b8bb631a2f
even44 <even.andreas.ingeberg@ebnett.no>
2025-02-27 16:09:58 +0100

    added ballls and balls in cages

---
+++ scripts/game_objects/bouncy_ball.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+@export var bouncy_boost: float = 400
+
+@export var vel_limit: float = 45
+
+@onready var ball: RigidBody3D = $RigidBody3D
+
+
+
+func _on_ball_body_entered(_body:Node):
+	var vel = ball.linear_velocity
+	print(vel.length())
+	if vel.length() < 60:
+		vel = vel.normalized()
+		ball.apply_impulse(vel * bouncy_boost)
+	
